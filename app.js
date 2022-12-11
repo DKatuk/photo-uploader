9bddb543111d9184e33665b563638c05400ef91f
DKatuk <dilarakatuk@gmail.com>
2022-12-11 15:03:27 +0300

    Learning middlewares

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 const express = require('express');
 const app = express();
 
+// MIDDLEWARES
+const myLogger = (req, res, next) => {
+    console.log('Middleware Log 1');
+    next();
+};
+
+const myLogger2 = (req, res, next) => {
+  console.log('Middleware Log 2');
+  next();
+};
+
+// Run middlewares
+app.use(express.static('public'));
+app.use(myLogger);
+app.use(myLogger2);
+
 app.get('/', (req, res) => {
     const photo = {
         id: 1,
