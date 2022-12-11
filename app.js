98e3ad4c53237266412a2649073f70169957dbfa
DKatuk <dilarakatuk@gmail.com>
2022-12-11 11:40:35 +0300

    Project structure with Expressjs get request

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
-const fn = function () {
-  console.log('Hello World');
-};
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    const photo = {
+        id: 1,
+        name: "Photo Name",
+        description: "Photo Description",
+    }
+    res.send(photo);
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
