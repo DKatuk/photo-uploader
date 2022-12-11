cc1f56f3e2f57d8c1a980d8e3e0e0ef25442f806
DKatuk <dilarakatuk@gmail.com>
2022-12-11 17:36:33 +0300

    clean template engine codes

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const ejs = require('ejs');
 const app = express();
-const path = require('path');
 
 // TEMPLATE ENGINE
 app.set('view engine', 'ejs');
@@ -11,19 +10,17 @@ app.use(express.static('public'));
 
 //ROUTES
 app.get('/', (req, res) => {
-  //res.sendFile(path.join(__dirname, 'temp/index.html'));
   res.render('index');
 });
 
 app.get('/about', (req, res) => {
-  //res.sendFile(path.join(__dirname, 'temp/index.html'));
   res.render('about');
 });
 
 app.get('/add', (req, res) => {
-  //res.sendFile(path.join(__dirname, 'temp/index.html'));
   res.render('add');
 });
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
